468d6667457e1ef359a2039a1a12a25bb4e7b4a5
angel.romero <angel.romero@ct.uneatlantico.es>
2025-11-13 13:13:47 +0100

    Testeando endpoints en hosting

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,36 @@
 import DemoWrapper from "@/components/DemoWrapper";
+import { useEffect, useState } from "react";
 
-export default function Home() {
+
+
+function Home() {
+  const [items, setItems] = useState<string[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch('https://test.miocafehn.com/api/get-data.php');
+        const data = await res.json();
+        setItems(data.items);
+      } catch (err) {
+        console.error('Error fetching data:', err);
+      }
+    }
+    fetchData();
+  }, []);
+
+
   return (
     <main className="flex min-h-screen items-center justify-center">
+      <div>
+        <h1>Items desde Freehostia</h1>
+        <ul>
+          {items.map(item => <li key={item.id}>{item.name}</li>)}
+        </ul>
+      </div>
       <DemoWrapper />
     </main>
   );
 }
+
+export default Home;
